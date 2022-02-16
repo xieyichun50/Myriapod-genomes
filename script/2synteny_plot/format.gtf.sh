c938628627c22e8e0786c9788c567ab382fb52c7
Yichun <59522685+xieyichun50@users.noreply.github.com>
2022-02-16 18:40:18 +0800

    Update format.gtf.sh

--- script/2synteny_plot/format.gtf.sh
+++ script/2synteny_plot/format.gtf.sh
@@ -1,4 +1,4 @@
-cat /jelly_data/yichun/snail/longest_protO/specieslist | while read i;
+cat specieslist | while read i;
 do 
-Rscript /jelly_data/yichun/scripts/2synteny/orthofinder_gtf_formatting.R -i $i.longest-gene.gtf 
+Rscript /scripts/2synteny/orthofinder_gtf_formatting.R -i $i.longest-gene.gtf 
 done
