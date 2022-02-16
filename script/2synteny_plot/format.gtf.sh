9fd54a4a85604b521b6741e864492143a127f043
Yichun <59522685+xieyichun50@users.noreply.github.com>
2022-02-16 18:41:17 +0800

    Update format.gtf.sh

--- script/2synteny_plot/format.gtf.sh
+++ script/2synteny_plot/format.gtf.sh
@@ -1,4 +1,4 @@
 cat specieslist | while read i;
 do 
-Rscript /scripts/2synteny/orthofinder_gtf_formatting.R -i $i.longest-gene.gtf 
+Rscript /2synteny/orthofinder_gtf_formatting.R -i $i.longest-gene.gtf 
 done
