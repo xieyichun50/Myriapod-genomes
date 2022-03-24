7b6b7e0547ab1e6fcdea9a5edfa3d9d4879a23c6
Yichun <59522685+xieyichun50@users.noreply.github.com>
2022-03-24 20:30:08 +0800

    Update 1prepcafe.sh

--- script/3gene_family_summary/1prepcafe.sh
+++ script/3gene_family_summary/1prepcafe.sh
@@ -8,5 +8,6 @@ ls -l Orthogroups/Orthogroups.GeneCount.tsv
 samtools faidx SpeciesTreeAlignment.fa
 cat SpeciesTreeAlignment.fa.fai
 
+python2 /tools/CAFE5/python_scripts/cafetutorial_prep_r8s.py -i SpeciesTree_rooted_node_labels.txt -o r8s_ctl_file.txt -s 138195 -p 'Caenorhabditis_elegans,Homo_sapiens' -c '736'
 awk 'OFS="\t" {$NF=""; print}' Orthogroups.GeneCount.tsv > tmp && awk '{print "(null)""\t"$0}' tmp > cafe.input.tsv && sed -i '1s/(null)/Desc/g' cafe.input.tsv && rm tmp
 python3 /tools/CAFE5/python_scripts/cafetutorial_clade_and_size_filter.py -i cafe.input.tsv -o cafe.input.filter.tsv -s
